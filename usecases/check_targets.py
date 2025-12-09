6a5319db32d7da334c0d639bc3baf567d0d6e226
Aleksei Khatkevich <qwerty12345@disroot.org>
2025-12-09 13:35:19 +0300

    [CP] 23

--- usecases/check_targets.py
+++ usecases/check_targets.py
@@ -4,15 +4,28 @@ from adapter.http_price_sourcing_repository import repository as http_repository
 from adapter.sql_price_repository import SQLPriceRepository
 from domain.price import CryptoPrice
 
+from typing import AsyncGenerator, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from domain.price_sourcing_repository import CryptoPriceSourcingRepository
+    from domain.price_db_repository import CryptoPriceRepository
+
 
 # DI !
+# StructLog
+#
 QUEUE_MAXSIZE = 3
 
 class CheckTargetsUseCase:
 
-    def __init__(self, targets_repo = http_repository, sql_repo=SQLPriceRepository):
-        self.targets_repo = targets_repo()
-        self.sql_repo = sql_repo()
+    def __init__(
+            self,
+            targets_repo:'CryptoPriceSourcingRepository' = http_repository,
+            sql_repo: 'CryptoPriceRepository'=SQLPriceRepository(),
+    ) -> None:
+        self.targets_repo = targets_repo
+        self.sql_repo = sql_repo
+
         self.fetch_queue = asyncio.Queue(QUEUE_MAXSIZE)
         self.telegram_queue = asyncio.Queue(QUEUE_MAXSIZE)
         self.save_db_queue = asyncio.Queue(QUEUE_MAXSIZE)
@@ -22,7 +35,7 @@ class CheckTargetsUseCase:
         self.save_in_db_end_event = asyncio.Event()
         self.tg_end_event = asyncio.Event()
 
-    async def send_to_tg_consumer(self, _id):
+    async def send_to_tg_consumer(self, _id: int) -> None:
         async for price in self._get_price_from_queue(
                 self.telegram_queue,
                 self.fetch_end_event,
@@ -33,16 +46,24 @@ class CheckTargetsUseCase:
             print(f'{_id=} send_to_tg_consumer:: Sending price {price} to telegram bot.')
             self.telegram_queue.task_done()
 
-
-    async def check_trigger(self, price: CryptoPrice):
+# last. saved is None ?
+    async def check_trigger(self, price: CryptoPrice) -> None:
         if ((price.movement_direction.DOWN and price.last_saved > price.target >= price.current) or
                 (price.movement_direction.UP and price.last_saved < price.target <= price.current)):
             price.is_active = False
             await self.telegram_queue.put(price)
+            print(f'check_trigger:: Condition has triggered. Target -- {price.target}, price -- {price.current}')
+
 
 
     @staticmethod
-    async def _get_price_from_queue(queue, prev_event, current_event, _id, name):
+    async def _get_price_from_queue(
+            queue: asyncio.Queue,
+            prev_event: asyncio.Event,
+            current_event: asyncio.Event,
+            _id: int,
+            name: str,
+    ) -> AsyncGenerator[CryptoPrice, None]:
         """
         1) С таймаутом в 1 сек мы пытаемся получить задачу из очереди пока продюсер не закончил работу.
         2) Как только продюсер закончил работу prev_event.is_set() == True, мы переходим в режим get_nowait,
@@ -68,7 +89,7 @@ class CheckTargetsUseCase:
             else:
                 yield price
 
-    async def fetch_web_data_consumer(self, _id):
+    async def fetch_web_data_consumer(self, _id: int) -> None:
         async for price in self._get_price_from_queue(
                 self.fetch_queue,
                 self.main_end_event,
@@ -81,7 +102,7 @@ class CheckTargetsUseCase:
             await self.save_db_queue.put(price)
             self.fetch_queue.task_done()
 
-    async def save_data_in_db_consumer(self, _id):
+    async def save_data_in_db_consumer(self, _id: int) -> None:
         async for price in self._get_price_from_queue(
                 self.save_db_queue,
                 self.fetch_end_event,
@@ -93,20 +114,17 @@ class CheckTargetsUseCase:
             self.save_db_queue.task_done()
 
 
-    async def main_producer(self):
+    async def main_producer(self) -> None:
         async for price in self.sql_repo:
             await self.fetch_queue.put(price)
             print(f'main_producer::Put price {price} in queue')
         self.main_end_event.set()
         print(f'main_producer::Set main event. All done!')
 
-    async def execute(self):
+    async def execute(self) -> None:
         await asyncio.gather(
             self.main_producer(),
-            *[self.fetch_web_data_consumer(_id) for _id in range(3)],
-            *[self.save_data_in_db_consumer(_id) for _id in range(3)],
-            *[self.send_to_tg_consumer(_id) for _id in range(3)],
+            *[self.fetch_web_data_consumer(_id) for _id in range(QUEUE_MAXSIZE)],
+            *[self.save_data_in_db_consumer(_id) for _id in range(QUEUE_MAXSIZE)],
+            *[self.send_to_tg_consumer(_id) for _id in range(QUEUE_MAXSIZE)],
         )
-
-
-
