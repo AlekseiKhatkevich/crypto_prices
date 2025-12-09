198572e9f0f1b6920af2bfc96e8d4b2f0cd0ceed
Aleksei Khatkevich <qwerty12345@disroot.org>
2025-12-09 18:09:50 +0300

    [CP] 29

--- adapter/price_tg_repository.py
+++ adapter/price_tg_repository.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 from telethon import TelegramClient
 
-from _secrets import telegram_api_hash, telegram_api_id, telegram_bot_token
+from _secrets import telegram_api_hash, telegram_api_id, telegram_bot_token, telegram_user_id
 from domain.price_tg_repository import PriceMessangerRepository
 
 if TYPE_CHECKING:
@@ -17,4 +17,5 @@ class PriceTGRepository(PriceMessangerRepository):
         message = (f'Цена на {price.standard_name.upper()} пересекла таргет {price.target}. {os.linesep}'
                        f'Текущая цена {price.current}.')
         bot = await TelegramClient('bot', telegram_api_id, telegram_api_hash).start(bot_token=telegram_bot_token)
-        await bot.send_message(5282738353, message=message)
+        async with bot as session:
+            await session.send_message(telegram_user_id, message=message)
